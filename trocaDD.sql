4f2a0462682ab75064be4cf4a2e11bdf6cf5579c
José Victor Munhoz <josevmunhoz@gmail.com>
2016-10-25 22:41:38 -0200

    Adicionado procedures de insertTroca updateData e alterConcretizacao na package trocaDD.

--- trocaDD.sql
+++ trocaDD.sql
@@ -1,4 +1,31 @@
 create or replace package trocaDD as
+-- PACKAGE PARA TRATAMENTO DOS DADOS NA TABELA TROCA.
 --
+--
+-- PROCEDURE DE INSERT NA TABELA TROCA.
+  procedure insertTroca (
+    pIdTrocadorA    IN  troca.id_trocadorA%TYPE,
+    pIdTrocadorB    IN  troca.id_trocadorB%TYPE,
+    pData           IN  troca.data_concretizada%TYPE,
+    concretizacaoA  IN  troca.concretizacaoTrocadorA%TYPE,
+    concretizacaoB  IN  troca.concretizacaoTrocadorB%TYPE,
+  );
+  --
+  --PROCEDURE DE UPDATE DATA
+  --
+  procedure updateData (
+    pIdTroca  IN  troca.id%TYPE,
+    pData     IN  troca.data_concretizada%TYPE
+  );
+  --
+  --PROCEDURE DE UPDATE CONCRETIZACAO
+  --
+  procedure alterConcretizacao (
+    -- Genérica, voce envia A ou B no IdConcretizador e o valor de como deve
+    --   ficar.
+    pIdTroca          IN  troca.id%TYPE,
+    pIdConcretizador  IN  varchar2,
+    pIndConcretizacao IN  troca.concretizacaoTrocadorA%TYPE
+  );
 --
 end trocaDD;
--- trocaDDBody.sql
+++ trocaDDBody.sql
@@ -1,5 +1,103 @@
 create or replace package body trocaDD as
 --
-
+  procedure insertTroca(
+    pIdTrocadorA    IN  troca.id_trocadorA%TYPE,
+    pIdTrocadorB    IN  troca.id_trocadorB%TYPE,
+    pData           IN  troca.data_concretizada%TYPE,
+    concretizacaoA  IN  troca.concretizacaoTrocadorA%TYPE,
+    concretizacaoB  IN  troca.concretizacaoTrocadorB%TYPE,
+  ) as
+    procedureName varchar2 := 'insertTroca';
+  begin
+    --
+    -- log de entrada
+    --
+    inicioProcedure_DBMS(procedureName, 'O');
+    --
+    insert into troca
+    values (seq_troca.nextval, pIdTrocadorA, pIdTrocadorB, pData, concretizacaoA, concretizacaoB);
+    --
+    -- log de saida
+    --
+    fimProcedure_DBMS(procedureName, 'O');
+    --
+  exception
+    when others then
+      --
+      null;
+      --
+  end;
+  --
+  --PROCEDURE DE UPDATE DA DATA.
+  --
+  procedure updateData (
+    pIdTroca  IN  troca.id%TYPE,
+    pNovaData     IN  troca.data_concretizada%TYPE
+  ) as
+    procedureName varchar2 := 'updateData';
+  begin
+    --
+    -- log de entrada
+    --
+    inicioProcedure_DBMS(procedureName, 'O');
+    --
+    update troca t
+    set    t.data_concretizada = pNovaData
+    where  t.id = pIdTroca;
+    --
+    -- log de saida
+    --
+    fimProcedure_DBMS(procedureName, 'O');
+    --
+  exception
+    when others then
+      --
+      null;
+      --
+  end;
+  --
+  --PROCEDURE DE UPDATE CONCRETIZACAO
+  --
+  procedure alterConcretizacao (
+    -- Genérica, voce envia A ou B no IdConcretizador e o valor de como deve
+    --   ficar.
+    pIdTroca          IN  troca.id%TYPE,
+    pIdConcretizador  IN  varchar2,
+    pIndConcretizacao IN  troca.concretizacaoTrocadorA%TYPE
+  ) as
+    procedureName varchar2 := 'alterConcretizacao';
+  begin
+    --
+    -- log de entrada
+    --
+    inicioProcedure_DBMS(procedureName, 'O');
+    --
+    if pIdConcretizador = 'A' then
+      --
+      update troca t
+      set    t.concretizacaoTrocadorA = pIndConcretizacao
+      where  t.id = pIdTroca;
+      --
+      scambioLog_DBMS(procedureName, 'Alterado concretizacaoTrocadorA <= '|| pIndConcretizacao, 'U');
+      --
+    else
+      --
+      update troca t
+      set    t.concretizacaoTrocadorB = pIndConcretizacao
+      where  t.id = pIdTroca;
+      --
+      scambioLog_DBMS(procedureName, 'Alterado concretizacaoTrocadorB <= '|| pIndConcretizacao, 'U');
+      --
+    end if;
+    -- log de saida
+    --
+    fimProcedure_DBMS(procedureName, 'O');
+    --
+  exception
+    when others then
+      --
+      null;
+      --
+  end;
 --
 end trocaDD;
