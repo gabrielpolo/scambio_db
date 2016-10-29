e2e6b49cae582c5c4f55b5a466932ac44dbb3b3f
José Victor Munhoz <josevmunhoz@gmail.com>
2016-10-29 20:47:26 -0200

    Adicionado sequence para o id do endereços.

--- sequences.sql
+++ sequences.sql
@@ -1,6 +1,7 @@
 drop sequence log_sequence;
 drop sequence seq_resp;
 drop sequence seq_troca;
+drop sequence seq_endereco;
 -- ARQUIVO RESPONSAVEL PELAS SEQUENCES DOS OBJETOS DE DADOS.
 --
 -- SEQUENCE DO ID DO LOG.
@@ -19,6 +20,13 @@ create sequence seq_resp
 --
 --SEQUENCE DO ID DA TROCA
 create sequence seq_troca
+  increment by 1
+  NOCACHE
+  NOCYCLE;
+--
+--
+--SEQUENCE DO ID DA TROCA
+create sequence seq_endereco
   increment by 1
   NOCACHE
   NOCYCLE;
