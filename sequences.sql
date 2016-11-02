09d038c8c84dc27f59d73d2042f6a86c0aac99bd
José Victor Munhoz <josevmunhoz@gmail.com>
2016-11-02 18:05:38 -0200

    Adicionado sequence referente ao id da tabela item.

--- sequences.sql
+++ sequences.sql
@@ -2,6 +2,7 @@ drop sequence log_sequence;
 drop sequence seq_resp;
 drop sequence seq_troca;
 drop sequence seq_endereco;
+drop sequence seq_item;
 -- ARQUIVO RESPONSAVEL PELAS SEQUENCES DOS OBJETOS DE DADOS.
 --
 -- SEQUENCE DO ID DO LOG.
@@ -30,4 +31,10 @@ create sequence seq_endereco
   increment by 1
   NOCACHE
   NOCYCLE;
-/
+--
+--
+--SEQUENCE DO ID DO ITEM
+create sequence seq_item
+  increment by 1
+  NOCACHE
+  NOCYCLE;
