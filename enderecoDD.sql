8eeb80028ac35be0a06c5f1f45ccaba6d6bed8d3
José Victor Munhoz <josevmunhoz@gmail.com>
2016-11-02 11:56:18 -0200

    Adicionado Insert de endereço.

--- enderecoDD.sql
+++ enderecoDD.sql
@@ -3,6 +3,14 @@ create or replace package enderecoDD as
 -- PROCEDURE DE DADOS DA TABELA ENDERECO.
 --
   procedure insertEndereco (
-    
+    pinLogradouro  IN  endereco.logradouro%TYPE,
+    pinNumero      IN  endereco.numero%TYPE,
+    pinCep         IN  endereco.cep%TYPE,
+    pinComplemento IN  endereco.complemento%TYPE,
+    pinTipo        IN  endereco.tipo%TYPE,
+    pinCidade      IN  endereco.cidade%TYPE,
+    pinEstado      IN  endereco.estado%TYPE,
+    pinResponsavel IN  endereco.responsavel%TYPE
   );
+  --
 end enderecoDD;
--- enderecoDDBody.sql
+++ enderecoDDBody.sql
@@ -1,3 +1,30 @@
 create or replace package body enderecoDD as
-
+--
+--
+  procedure insertEndereco (
+    pinLogradouro  IN  endereco.logradouro%TYPE,
+    pinNumero      IN  endereco.numero%TYPE,
+    pinCep         IN  endereco.cep%TYPE,
+    pinComplemento IN  endereco.complemento%TYPE,
+    pinTipo        IN  endereco.tipo%TYPE,
+    pinCidade      IN  endereco.cidade%TYPE,
+    pinEstado      IN  endereco.estado%TYPE,
+    pinResponsavel IN  endereco.responsavel%TYPE
+  )
+  as
+  --
+  procedureName varchar2(30) := 'insertEndereco';
+  --
+  Begin
+  --
+  logProcedures.inicioProcedure_DBMS(procedureName, 'O');
+  --
+  insert into endereco
+    values (seq_endereco.nextval, pinLogradouro, pinNumero, pinCep, pinComplemento,
+             pinTipo, pinCidade, pinEstado, pinResponsavel );
+  --
+  logProcedures.fimProcedure_DBMS(procedureName, 'O');
+  --
+  end;
+--
 end enderecoDD;
