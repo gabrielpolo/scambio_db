2a3ea292d3d111737b9b4d3a37bff3c4371ec292
José Victor Munhoz <josevmunhoz@gmail.com>
2016-11-23 01:31:32 -0200

    Adicionado functions basicas para o funcionamento da feature de troca

--- trocaDD.sql
+++ trocaDD.sql
@@ -1,15 +1,41 @@
 create or replace package trocaDD as
 -- PACKAGE PARA TRATAMENTO DOS DADOS NA TABELA TROCA.
 --
---
--- PROCEDURE DE INSERT NA TABELA TROCA.
-  procedure insertTroca (
+  function completaTroca (
+    pinItemB    IN item.id%TYPE,
+    pinIdTroca  IN troca.id%TYPE,
+    pinRelTroca IN rel_troca_item.id%TYPE
+  ) return boolean;
+  --
+  -- BUSCA ID EVENTO RELAÇÃO DE TROCA.
+  --
+  function buscaIdRelTrocaItem (
+    pinIdTroca  IN troca.id%TYPE
+  ) return rel_troca_item.id%TYPE;
+  --
+  -- FUNCTION QUE BUSCA ID DA TROCA
+  --
+  function buscaIdTrocaPorCompletar (
+    pinIdDono         IN responsavel.id%TYPE,
+    pinIdItemOriginal IN item.id%TYPE
+  ) return troca.id%TYPE;
+  --
+  -- FUNCTION DE INSERT NA TABELA TROCA.
+  --
+  function insertTroca(
     pIdTrocadorA    IN  troca.id_trocadorA%TYPE,
     pIdTrocadorB    IN  troca.id_trocadorB%TYPE,
     pData           IN  troca.data_concretizada%TYPE,
     concretizacaoA  IN  troca.concretizacaoTrocadorA%TYPE,
     concretizacaoB  IN  troca.concretizacaoTrocadorB%TYPE
-  );
+  ) return troca.id%TYPE;
+  --
+  -- PROCEDURE DE INSERT NA TABELA DE REL_TROCA_ITEM
+  --
+  function insertRelacaoTroca (
+    pinIdItem  IN item.id%TYPE,
+    pinIdTroca IN troca.id%TYPE
+  ) return rel_troca_item.id%TYPE;
   --
   --PROCEDURE DE UPDATE DATA
   --
--- trocaDDBody.sql
+++ trocaDDBody.sql
@@ -1,14 +1,125 @@
 create or replace package body trocaDD as
 --
-  procedure insertTroca(
+  function completaTroca (
+    pinItemB    IN item.id%TYPE,
+    pinIdTroca  IN troca.id%TYPE,
+    pinRelTroca IN rel_troca_item.id%TYPE
+  ) return boolean as
+    --
+    procedureName varchar2(30) := 'completaTroca';
+    --
+  begin
+    --
+    logProcedures.inicioProcedure_DBMS(procedureName, 'O');
+    -- Atualizamos a data da troca.
+    updateData(pinIdTroca, sysdate);
+    -- Atualizamos o status da aprovação.
+    alterConcretizacao(pinIdTroca, 'A', 'S');
+    --
+    update  rel_troca_item rel
+      set   rel.id_item_b = pinItemB
+      where rel.id = pinIdTroca;
+    --
+    logProcedures.fimProcedure_DBMS(procedureName, 'O');
+    --
+    return true;
+  exception
+    when others then
+      return false;
+  end;
+--
+  function buscaIdRelTrocaItem (
+    pinIdTroca  IN troca.id%TYPE
+  ) return rel_troca_item.id%TYPE as
+    --
+    procedureName varchar2(30) := 'buscaIdRelTrocaItem';
+    --
+    vnIdRelTrocaItem rel_troca_item.id%TYPE;
+    --
+  begin
+    --
+    logProcedures.inicioProcedure_DBMS(procedureName, 'O');
+    --
+    select rel.id
+    into   vnIdRelTrocaItem
+    from   rel_troca_item rel
+    where  rel.id = pinIdTroca;
+    --
+    logProcedures.fimProcedure_DBMS(procedureName, 'O');
+    --
+    return vnIdRelTrocaItem;
+    --
+  exception
+    when others then
+      return null;
+  end;
+--
+  function buscaIdTrocaPorCompletar (
+    pinIdDono         IN responsavel.id%TYPE,
+    pinIdItemOriginal IN item.id%TYPE
+  ) return troca.id%TYPE as
+   --
+   procedureName varchar2(30) := 'buscaIdTrocaPorCompletar';
+   --
+   vnIdTroca     troca.id%TYPE;
+   --
+  begin
+    --
+    logProcedures.inicioProcedure_DBMS(procedureName, 'O');
+    --
+    select t.id
+    into   vnIdTroca
+    from   troca t,
+           rel_troca_item rel
+    where  t.id_trocadorA           = pinIdDono           and
+           t.concretizacaoTrocadorA = 'N'                 and
+           rel.id_troca             = t.id                and
+           rel.id_item_a            = pinIdItemOriginal;
+    --
+    logProcedures.fimProcedure_DBMS(procedureName, 'O');
+    --
+    return vnIdTroca;
+    --
+  exception
+    when others then
+      return null;
+  end;
+--
+  function insertRelacaoTroca (
+    pinIdItem  IN item.id%TYPE,
+    pinIdTroca IN troca.id%TYPE
+  ) return rel_troca_item.id%TYPE as
+  --
+  procedureName varchar2(30) := 'insertRelacaoTroca';
+  --
+  vnIdEventoRel number := seq_rel_troca_item.nextval;
+  --
+  begin
+    --
+    logProcedures.inicioProcedure_DBMS(procedureName, 'O');
+    --
+    insert into rel_troca_item
+      values (vnIdEventoRel, pinIdItem, null, pinIdTroca);
+    --
+    commit;
+    --
+    logProcedures.fimProcedure_DBMS(procedureName, 'O');
+    --
+    return vnIdEventoRel;
+    --
+  end;
+--
+  function insertTroca(
     pIdTrocadorA    IN  troca.id_trocadorA%TYPE,
     pIdTrocadorB    IN  troca.id_trocadorB%TYPE,
     pData           IN  troca.data_concretizada%TYPE,
     concretizacaoA  IN  troca.concretizacaoTrocadorA%TYPE,
     concretizacaoB  IN  troca.concretizacaoTrocadorB%TYPE
-  ) as
+  ) return troca.id%TYPE as
     --
     procedureName varchar2(30) := 'insertTroca';
+    --
+    vnIdTroca troca.id%TYPE;
     --
   begin
     --
@@ -16,14 +127,18 @@ create or replace package body trocaDD as
     --
     logProcedures.inicioProcedure_DBMS(procedureName, 'O');
     --
+    vnIdTroca := seq_troca.nextval;
+    --
     insert into troca
-    values (seq_troca.nextval, pIdTrocadorA, pIdTrocadorB, pData, concretizacaoA, concretizacaoB);
+    values (vnIdTroca, pIdTrocadorA, pIdTrocadorB, pData, concretizacaoA, concretizacaoB);
     --
     -- log de saida
     --
     logProcedures.fimProcedure_DBMS(procedureName, 'O');
     --
     commit;
+    --
+    return vnIdTroca;
     --
   end;
   --
